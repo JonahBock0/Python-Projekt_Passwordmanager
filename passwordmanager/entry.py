52dbddd7d5c0f3c7598fd8731636effaff5e6109
JonahBock0 <jonah.bock@stud.hawk.de>
2020-04-24 14:02:43 +0200

    Erste Struktur erstellt

---
+++ passwordmanager/entry.py
@@ -0,0 +1,7 @@
+class Entry:
+    def __init__(self):
+        self.name = ""
+        self.user = ""
+        self.password = ""
+        self.notes = ""
+        self.attributes = dict()
---
+++ passwordmanager/gui.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+
+def gui(manager):
+    root = Tk()
+    root.title("Passwortmanager")
+    # a = Label(text="Test")
+    # a.grid()
+    root.mainloop()
