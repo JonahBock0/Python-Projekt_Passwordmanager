9641fbb27238be8270eb3dab80a51f08a9874186
JonahBock0 <jonah.bock@stud.hawk.de>
2020-05-01 13:18:14 +0200

    manager: init (mit from_string), entries_from_string und to_string hinzugefügt

--- passwordmanager/manager.py
+++ passwordmanager/manager.py
@@ -1,3 +1,13 @@
+def entries_from_string(from_string):
+    return []
+
+
 class Manager:
     def __init__(self):
         self.entries = []
+
+    def __init__(self, from_string):
+        self.entries = entries_from_string(from_string)
+
+    def to_string(self):
+        return ""
