19ce38dad8cacd88fc11dea7aec665401a260380
JonahBock0 <jonah.bock@stud.hawk.de>
2020-05-01 13:48:59 +0200

    cli umstrukturiert

--- passwordmanager/cli.py
+++ passwordmanager/cli.py
@@ -1,57 +1,52 @@
-import sys, getpass
+import getpass
+from passwordmanager.manager import Manager
 
-MASTERPW = "HAWK2020"
-PASSWORD = getpass.getpass("Master-Passwort eingeben: ")
-
-while MASTERPW != PASSWORD:
-    if MASTERPW != PASSWORD:
-        print("Invalid Password\n")
-        break
-if MASTERPW == PASSWORD:
-    print("Password correct\n")
-
-
-    def Untermenü():
-        auswahl_Untermenü = int(input('Untermenü:\nWählen Sie eine der folgenden Funktionen:\
-                     \n\n\t1. Eintrag bearbeiten\n\t2. Eintrag löschen\n\t3. zurück ins Hauptmenü\n\nIhre Eingabe: '))
-        switch_Untermenü = {1: Eintrag_bearbeiten, 2: Eintrag_löschen}
-        funktion_Untermenü = switch_Untermenü.get(auswahl_Untermenü, fehler)
-        funktion_Untermenü()
+def Untermenü(manager):
+    auswahl = int(input('Untermenü:\nWählen Sie eine der folgenden Funktionen:\
+                 \n\n\t1. Eintrag bearbeiten\n\t2. Eintrag löschen\n\t3. zurück ins Hauptmenü\n\nIhre Eingabe: '))
+    if auswahl == 3:
+        return
+    funktionen = {1: Eintrag_bearbeiten, 2: Eintrag_löschen}
+    funktion = funktionen.get(auswahl, fehler)
+    funktion(manager)
 
 
-    def Eintrag_löschen():
-        print('Eintrag löschen:\n')
+def Eintrag_löschen(manager):
+    print('Eintrag löschen:\n')
 
 
-    def Eintrag_bearbeiten():
-        print('Eintrag_bearbeiten\n')
+def Eintrag_bearbeiten(manager):
+    print('Eintrag_bearbeiten\n')
 
 
-    def Eintrag_hinzufügen(service, password):
-        print('Eintrag hinzufügen:n')
-        Untermenü()
+def Eintrag_hinzufügen(manager):
+    print('Eintrag hinzufügen:n')
+    Untermenü(manager)
 
 
-    def Eintrag_anzeigen():
-        print('Eintrag Anzeigen:\n')
-        Untermenü()
+def Eintrag_anzeigen(manager):
+    print('Eintrag Anzeigen:\n')
+    Untermenü(manager)
 
 
-    def Programm_verlassen():
-        print('Programm_verlassen:\n')
-        sys.exit()
+def fehler(manager):
+    print("Die eingegebene Zahl ist nicht im Menü")
 
 
-    def fehler():
-        print("Die eingegebene Zahl ist nicht im Menü")
-
-
-    def cli(manager):
-        auswahl_Hauptmenü = int(input('Willkommen im Passwordmanager\n\nHauptmenü:\nWählen Sie eine der folgenden Funktionen:\
+def menu(manager):
+    while True:
+        auswahl = int(input('Willkommen im Passwordmanager\n\nHauptmenü:\nWählen Sie eine der folgenden Funktionen:\
                                 \n\n\t\t1. Eintrag hinzufügen\n\t\t2. Eintrag anzeigen\n\t\t3. Programm verlassen\n\nIhre Eingabe: '))
-        switch_Hauptmenü = {1: Eintrag_hinzufügen, 2: Eintrag_anzeigen, 3: Programm_verlassen}
-        funktion_Hauptmenü = switch_Hauptmenü.get(auswahl_Hauptmenü, fehler)
-        funktion_Hauptmenü()
+        if auswahl == 3:
+            break
+        funktionen = {1: Eintrag_hinzufügen, 2: Eintrag_anzeigen}
+        funktion = funktionen.get(auswahl, fehler)
+        funktion(manager)
 
 
-    cli(0)
+def cli():
+    # Neue "Datenbank" erstellen oder einlesen
+    manager = Manager()
+    menu(manager)
+
+cli()
--- passwordmanager/manager.py
+++ passwordmanager/manager.py
@@ -6,8 +6,8 @@ class Manager:
     def __init__(self):
         self.entries = []
 
-    def __init__(self, from_string):
-        self.entries = entries_from_string(from_string)
+    # def __init__(self, from_string):
+    #    self.entries = entries_from_string(from_string)
 
     def to_string(self):
         return ""
