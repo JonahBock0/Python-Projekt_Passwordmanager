76a08a1b833b7931bc94b222979a1468b93795f3
Marvin.Wolf <marvin.wolf@stud.hawk.de>
2020-05-04 19:12:46 +0200

    crypt: generate_key ergänzt

--- passwordmanager/crypt.py
+++ passwordmanager/crypt.py
@@ -1,8 +1,23 @@
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 
 
 def generate_key(password: str):
-    pass
+    password_byte = password.encode()
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password_byte))
+    return key
 
 
 def encrypt(data_string: str, key: bytes):
