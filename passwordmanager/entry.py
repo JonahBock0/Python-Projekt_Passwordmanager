9894e9ed80e1e750fc545e377338a1ca569bf3aa
JonahBock0 <jonah.bock@stud.hawk.de>
2020-05-04 21:02:36 +0200

    entry: entry_from_string, to_string als Vorlage hinzugefügt, init erweitert

--- passwordmanager/entry.py
+++ passwordmanager/entry.py
@@ -1,7 +1,24 @@
+def entry_from_string(from_string: str):
+    name = ""
+    user = ""
+    password = ""
+    notes = ""
+    attributes = dict()
+    return name, user, password, notes, attributes
+
+
 class Entry:
-    def __init__(self):
-        self.name = ""
-        self.user = ""
-        self.password = ""
-        self.notes = ""
-        self.attributes = dict()
+    def __init__(self, from_string: str = None, name: str = "", user: str = "", password: str = ""):
+        if from_string:
+            self.name, self.user, self.password, self.notes, self.attributes = entry_from_string(from_string)
+        else:
+            self.name = name
+            self.user = user
+            self.password = password
+            self.notes = ""
+            self.attributes = dict()
+
+    def to_string(self):
+        lines = 0
+        string = ""
+        return string, lines
--- passwordmanager/manager.py
+++ passwordmanager/manager.py
@@ -1,9 +1,9 @@
-def entries_from_string(from_string):
+def entries_from_string(from_string: str):
     return []
 
 
 class Manager:
-    def __init__(self, from_string=None):
+    def __init__(self, from_string: str = None):
         self.entries = entries_from_string(from_string) if from_string else []
 
     def to_string(self):
